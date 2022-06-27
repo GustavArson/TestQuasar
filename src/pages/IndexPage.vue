64a39fec853223cd6c30e7b697c023b6aa9c8a05
GustavArson <loan.galluzzo@3wa.io>
2022-06-27 14:23:24 +0200

    test lawl

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page>
+    <h1>Hello World !</h1>
+    <p>Bonjour, je souhaiterai vous présentez ici bas la liste de nos restaurants</p>
+    <a href="#/PageTest.vue">Lien Test</a>
+    <table>
+      ici sera la liste des restaurants récupérés dans la bdd quand j'aurai réussi à le faire
+    </table>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
